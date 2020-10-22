feefc45cc58fc9ecf1a526480ca2c058b2be9754
Adria Gonzalez <adriagc8@pop-os.localdomain>
2020-10-22 19:07:24 +0200

    Console & Observable

---
+++ Practica 1/MVC/Console.java
@@ -0,0 +1,9 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class Console implements Observer {
+    @Override
+    public void update(Observable observable, Object obj) {
+       //sobresciurem el metode update per tal d'esciure per consola 
+    }
+}

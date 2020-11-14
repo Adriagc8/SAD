15513c9476cb4e22741c996743e8b4b87e3d9b71
unknown <rarinomateu@gmail.com>
2020-11-14 20:05:14 +0100

    Funcionalitat de historial per noves conexions

--- ChatPissarra/src/sockets.js
+++ ChatPissarra/src/sockets.js
@@ -4,11 +4,14 @@ module.exports = io => {
     let line_history = [];
   
     io.on('connection', socket => {
-        console.log('Nou usuari conectat');
+
+        for (let i in line_history) {
+            socket.emit('draw_line', {line: line_history[i]});
+        }
 
         socket.on('draw_line', data => {
             line_history.push(data.line);
-            io.emit('draw_line', data );
+            io.emit('draw_line', { line: data.line });
         });
     });
   

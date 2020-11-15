dda97b1796af90308ebb7e3d034b433c8f0355bf
Adria Gonzalez <adriagc8@pop-os.localdomain>
2020-11-15 18:38:02 +0100

    pencil, circle and square modes

--- ChatPissarra/src/public/index.js
+++ ChatPissarra/src/public/index.js
@@ -7,12 +7,16 @@ function init() {
         posFinal: false,
         pos_prev: false,
         circle:false,
-        square:false
+        square:false,
+        mode:"pencil"
     };
 
     // Canvas
     let canvas = document.getElementById('canvas');
     let clearButton = document.getElementById('clear-WB');
+    let pencilButton = document.getElementById('pencil');
+    let squareButton = document.getElementById('square');
+    let circleButton = document.getElementById('circle');
     let context = canvas.getContext('2d');
     let width = window.innerWidth;
     let height = window.innerHeight;
@@ -22,13 +26,28 @@ function init() {
     // Socket IO
     let socket = io();
     // Set the canvas width and height to the browser size
-    canvas.width = width/2;
-    canvas.height = height/2;
+    canvas.width = width *2/3;
+    canvas.height = height *2/3;
     
       
     clearButton.addEventListener('click', (e)=>{
         console.log('clear')
         socket.emit('clearAll');
+    })
+
+    pencilButton.addEventListener('click', (e)=>{
+        console.log('mode circle');
+        mouse.mode="pencil";
+    })
+
+    squareButton.addEventListener('click', (e)=>{
+        console.log('mode square');
+       mouse.mode="square";
+    })
+
+    circleButton.addEventListener('click', (e)=>{
+        console.log('mode circle');
+        mouse.mode="circle";
     })
 
 
@@ -81,11 +100,37 @@ function init() {
 
     //Funció principal recursiva
   function mainLoop() {
+      if(mouse.mode=="pencil"){
         if(mouse.click && mouse.move && mouse.pos_prev) {
             socket.emit('draw_line', { line: [mouse.pos, mouse.pos_prev] });
             mouse.move = false;
         }
         mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
+      }else if(mouse.mode=="circle"){
+        if(mouse.circle){
+            console.log("circle")
+           // socket.emit('draw_circle', { line: [mouse.pos, mouse.pos_prev] });
+            context.beginPath();
+            context.arc(mouse.pos.x* width,mouse.pos.y*height,30,0, Math.PI*2, false);
+            context.stroke();
+        }
+      }else if(mouse.mode=="square"){
+        if(mouse.square){
+            if(mouse.square){
+                console.log("square")
+                context.beginPath();
+                let widthS=mouse.posFinal.x-mouse.pos.x;
+                let heightS=mouse.posFinal.y-mouse.pos.y;
+                console.log(widthS)
+                console.log(heightS)
+               // context.strokeRect(mouse.pos.x* width,mouse.pos.y*height,widthS, heightS);
+                context.strokeRect(mouse.pos.x* width,mouse.pos.y*height,50, 50);
+                context.stroke();
+                mouse.square=false;
+            }
+        }
+      }
+      
         setTimeout(mainLoop, 25);
       }
     mainLoop();
@@ -101,8 +146,8 @@ function init() {
         }
         setTimeout(circle, 25);
     }
-    circle();
-    function square(){
+    circle();*/
+   /* function square(){
         //mouse.pos.x = e.clientX / width;
         
         if(mouse.square){

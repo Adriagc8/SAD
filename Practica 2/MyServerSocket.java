1bdf63a6b957948b868f5831ea85f62fbe729950
Adria Gonzalez <adriagc8@pop-os.localdomain>
2020-12-04 00:06:45 +0100

    Practica 2: MySocket & MyServerSocket

---
+++ Practica 2/MyServerSocket.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+
+
+public class MyServerSocket extends ServerSocket {
+    private ServerSocket serverSocket;
+    private MySocket socket;
+    public MyServerSocket(int port) throws IOException{
+
+        this.serverSocket = new ServerSocket(port);
+    }
+
+  
+    public MySocket Myaccept(){
+        try {
+            this.socket = new MySocket(serverSocket.accept()); //Listens for a connection to be made to this socket and accepts it. 
+                                                            //The method blocks until a connection is made.
+            return socket;
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+        return  null;
+    }
+
+    public void Myclose(){
+        try {
+            this.serverSocket.close(); //Closes this socket. Any thread currently blocked in accept() will throw a SocketException.
+                                        //If this socket has an associated channel then the channel is closed as well.
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}

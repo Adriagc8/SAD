917a3b042516ab77e9b126804acd28ef4d50d7db
Adria Gonzalez <adriagc8@pop-os.localdomain>
2020-12-12 00:36:07 +0100

    Primera implementacio del Server

--- Practica 2/Server.java
+++ Practica 2/Server.java
@@ -1,5 +1,36 @@
-
+import java.io.PrintWriter;
+import java.util.concurrent.Executors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Server {
+    private static final int PORT=5000;
+    public static ConcurrentHashMap<String, Handler> clients = new ConcurrentHashMap<String, Handler>();
+    public static void main(String[] args) throws Exception {
+        System.out.println("The server is running on port"+ PORT);
+        var pool = Executors.newFixedThreadPool(500);
+        try (MyServerSocket listener = new MyServerSocket(PORT)) {
+            while (true) {
+                pool.execute(new Handler(listener.accept()));
+            }
+        }
+    }
     
+    public static class Handler implements Runnable {
+        private final MySocket socket;
+        private String lastMsg; 
+        private String clientName;
+        public BufferedReader in = null;
+        public PrintWriter out = null;
+
+        public Handler(MySocket sc){
+            this.socket = sc;
+            this.in = new BufferedReader(new InputStreamReader(this.socket.MyGetInputStream()));
+            this.out = new PrintWriter(socket.MyGetOutputStream(), true);
+        }
+    }
 }
